645d468951bccab62bb1c9654d334e8fde5437be
Mohamad Al-Kahfi <91003256+KahfiSmith@users.noreply.github.com>
2025-11-05 15:14:00 +0700

    fix: correct import name for SkillsMarque and update Hero component styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import AboutIntro from "@/components/features/about/AboutIntro";
 import Hero from "@/components/features/home/Hero";
 import ProjectsList from "@/components/features/projects/ProjectsList";
-import Skillsmarque from "@/components/features/skills/SkillsMarquee";
+import SkillsMarque from "@/components/features/skills/SkillsMarquee";
 import WorkExperience from "@/components/features/works/WorkExperience";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Skillsmarque />
+      <SkillsMarque />
       <AboutIntro/>
       <WorkExperience/>      
       <ProjectsList/>
---
+++ src/components/common/index.ts
@@ -0,0 +1,3 @@
+export { default as Footer } from '@/components/common/Footer';
+export { default as SimpleCurtainLoader } from '@/components/common/SimpleCurtainLoader';
+export { default as SmoothScrollProvider } from '@/components/common/SmoothScrollProvider';
